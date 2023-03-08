74a980366313626bf4812f40dfb4aaabd9707aed
deadendpl <oliwier.czerwi7@gmail.com>
2023-03-08 17:31:15 +0100

    new file:   .config/alacritty/alacritty.yml 	new file:   .config/alacritty/dracula.yml 	new file:   .config/bspwm/bspwmrc 	new file:   .config/polybar/config.ini 	new file:   .config/polybar/config.inioryg 	new file:   .config/polybar/nate-sys.ini 	new file:   .config/polybar/start.sh 	new file:   .config/rofi/config.rasi 	new file:   .config/rofi/power.sh 	new file:   .config/rofi/web-search.sh 	new file:   .config/rofi/wifi.sh 	new file:   .config/sxhkd/brightness_down.sh 	new file:   .config/sxhkd/brightness_up.sh 	new file:   .config/sxhkd/sxhkdrc

---
+++ .config/sxhkd/brightness_up.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Get the argument
+fir_argument="increase"
+# Get the current brightness level
+brightness=$(xrandr --current --verbose | grep eDP-1 -A5 | grep -o -P '(?<=Brightness: ).*')
+# Decrease/increse the brightness by 5%
+if [ "$fir_argument" = "increase" ]; then
+new_brightness=$(echo "$brightness + 0.1" | bc)
+else
+new_brightness=$(echo "$brightness - 0.1" | bc)
+fi
+# Check if the new brightness level is above 1
+if [ "$(echo "$new_brightness > 1" | bc)" -eq 1 ]; then
+new_brightness=1
+fi
+# Check if the new brightness level is below 0
+if [ "$(echo "$new_brightness < 0" | bc)" -eq 1 ]; then
+new_brightness=0
+fi
+# Set the new brightness level
+xrandr --output eDP-1 --brightness $new_brightness
