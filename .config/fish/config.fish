62bade394050d56750b240c6b0bff58ff2d1b14f
deadendpl <oliwier.czerwi7@gmail.com>
2023-03-17 17:46:01 +0100

    modified:   .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+
+# aliases
+
+# apt
+alias inst='sudo apt install'
+alias pu='sudo apt purge'
+alias autopu='sudo apt autopurge'
+alias up='sudo apt update && sudo apt upgrade'
+alias upd='sudo apt update'
+alias upg='sudo apt upgrade'
+alias ainstalled='apt list --installed'
+
+
+alias ll='ls -la'
+alias clr='clear'
+
 end
