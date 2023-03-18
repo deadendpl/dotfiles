1627d0d3eba1ab0515e1ade0e2f32784007c9f5b
deadendpl <oliwier.czerwi7@gmail.com>
2023-03-18 13:17:16 +0100

    modified:   .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
 
+set PATH /home/oliwier/.config/emacs/bin $PATH
+
 # aliases
 
 # apt
