9caa1da6590d55595b9c26e090e7e950b61a9845
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-09 20:10:06 +0200

    stuff

--- .config/scripts/bookmarks.sh
+++ .config/scripts/bookmarks.sh
@@ -2,9 +2,10 @@
 
 file="$HOME/Documents/bookmarks"
 
+
 sort $file -o $file
 
-QUERY=$(cat $file | $launcher -dmenu -l 10 -p "Bookmarks")
+QUERY=$(cat $file | $launcher -dmenu -p Bookmarks:)
 
 if [ -n "$QUERY" ]; then
   xdg-open "${QUERY}" 2> /dev/null
