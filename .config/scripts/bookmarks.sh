05bdb86acda53e1f7b0a1da08085d8caed58cb16
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-11 10:38:39 +0200

    rofi themes

--- .config/scripts/bookmarks.sh
+++ .config/scripts/bookmarks.sh
@@ -4,7 +4,7 @@ file="$HOME/Documents/bookmarks"
 
 sort $file -o $file
 
-QUERY=$(cat $file | $launcher -dmenu -p Bookmarks)
+QUERY=$(cat $file | $launcher -theme ~/.config/rofi/themes/drac-list.rasi -dmenu -p Bookmarks)
 
 if [ -n "$QUERY" ]; then
   xdg-open "${QUERY}" 2> /dev/null
