9a10b5e74b40fca88e73c076fa71d3dcc981980a
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-25 18:50:40 +0200

    modified:   .config/hypr/hyprland.conf 	modified:   .config/scripts/web-search.sh 	modified:   .config/variety/.last_change_time 	modified:   .config/variety/history.txt 	modified:   .config/variety/variety.log 	modified:   .config/waybar/config

--- .config/scripts/web-search.sh
+++ .config/scripts/web-search.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-URL='https://www.phind.com/search?q='      # variable for search engine, you can change it if you want
-QUERY=$(echo '' | $launcher -theme ~/.config/rofi/themes/drac-list.rasi -dmenu -l 0 -b -p Search)
+declare -a search_engines=( "You" "Brave Search" "Google" "DuckDuckGo" "Bing" "Phind" "SearXNG" ) # add or remove search engines here
+
+selected_engine=$(printf '%s\n' "${search_engines[@]}" | sort | rofi -theme ~/.config/rofi/themes/drac-list.rasi -dmenu -l ${#search_engines[@]} -b -p "Search engine:")
+
+case $selected_engine in
+    "Google")
+        URL="https://www.google.com/search?q="
+        ;;
+    "DuckDuckGo")
+        URL="https://duckduckgo.com/?q="
+        ;;
+    "Bing")
+        URL="https://www.bing.com/search?q="
+        ;;
+    "Phind")
+        URL="https://www.phind.com/search?q="
+        ;;
+    "SearXNG")
+        URL="https://searx.mha.fi/search?q="
+        ;;
+    "Brave Search")
+        URL="https://search.brave.com/search?q="
+        ;;
+    "You")
+        URL="https://you.com/search?q="
+	;;
+    *)
+        exit 0
+        ;;
+esac
+
+QUERY=$(echo '' | rofi -theme ~/.config/rofi/themes/drac-list.rasi -dmenu -l 0 -b -p "Search $selected_engine:")
+
 if [ -n "$QUERY" ]; then
-  xdg-open "${URL}${QUERY}" 2> /dev/null
+    xdg-open "${URL}${QUERY}" 2> /dev/null
 fi
