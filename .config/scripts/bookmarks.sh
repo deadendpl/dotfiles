14302c25d62a5617375f2eaf244412bbe656219d
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-26 19:19:34 +0200

    modified:   .config/scripts/bookmarks.sh 	modified:   dependencies/dependencies-hypr.txt

--- .config/scripts/bookmarks.sh
+++ .config/scripts/bookmarks.sh
@@ -4,11 +4,11 @@ file="$HOME/Documents/bookmarks"
 
 sort $file -o $file
 
-options="Add new URL\n$(cat $file)"
+options="Enter other URL\n$(cat $file)"
 
 QUERY=$(echo -e "$options" | $launcher -theme ~/.config/rofi/themes/drac-list.rasi -dmenu -p Bookmarks)
 
-if [ "$QUERY" = "Add new URL" ]; then
+if [ "$QUERY" = "Enter other URL" ]; then
   QUERY=$(echo "" | $launcher -dmenu -p "Enter URL")
 fi
 
