3aa60e999f5a9697276953c6c07ab5c38ef06605
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-28 19:36:12 +0200

    modified:   .config/hypr/hyprland.conf 	modified:   .config/scripts/bookmarking.sh 	modified:   .config/variety/.last_change_time 	modified:   .config/variety/history.txt 	modified:   .config/variety/variety.log 	modified:   .config/variety/wallpaper/wallpaper.jpg.txt

--- .config/scripts/bookmarking.sh
+++ .config/scripts/bookmarking.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-bookmark="$(xclip -o)"
+if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+    clipboard_cmd="xclip -o"
+else
+    clipboard_cmd="wl-paste"
+fi
+
+bookmark="$($clipboard_cmd)"
 file="$HOME/Documents/bookmarks"
 
 if grep -q "^$bookmark$" "$file"; then
-	notify-send "Oopsie." "Already bookmarked!"
+    notify-send "Oopsie." "Already bookmarked!"
 else
-	notify-send "Bookmark added!" "$bookmark is now saved to the bookmarks."
-	echo "$bookmark" >> "$file"
+    notify-send "Bookmark added!" "$bookmark is now saved to the bookmarks."
+    echo "$bookmark" >> "$file"
 fi
+
