35c8ad6a9ea3a98a627183c6239e6decde3734b6
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-30 09:55:15 +0200

    modified:   install/bspwm.sh

--- install/bspwm.sh
+++ install/bspwm.sh
@@ -5,4 +5,16 @@ cd ../dependencies/bspwm/
 ./aur.sh
 
 cd ../../home/.config
-mv * ~/.config
+
+# Check if there are any directories present in ~/.config
+if [[ $(ls -d ~/.config/*/ 2>/dev/null) ]]; then
+  # Prompt the user before overwriting folders in ~/.config
+  read -rp "WARNING: ~/config directory is not empty. Proceed with overwriting existing files? [y/N] " confirm
+  if [[ ! "$confirm" =~ [Yy] ]]; then
+    echo "Operation aborted by user." >&2
+    exit 1
+  fi
+fi
+
+# Move all directories from the source directory to the destination directory
+mv -f * ~/.config/
