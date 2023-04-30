ddf98d28e55a24fae73aa3988ce2714d4be4d334
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-30 10:38:16 +0200

    added unified install script

---
+++ install/main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define the question and choices
+QUESTION="What would you like to install?"
+CHOICES=("bspwm" "" "off" "hyprland" "" "off")
+
+# Pipe the question and choices into whiptail
+OPTIONS=$(whiptail --title "Installation" --separate-output --checklist "$QUESTION" 15 60 2 \
+    "bspwm" "Install bspwm" ON \
+    "hyprland" "Install hyprland" OFF \
+    3>&1 1>&2 2>&3)
+
+# If the user selects bspwm, execute the bspwm.sh script
+if echo "$OPTIONS" | grep -q "bspwm"; then
+  exec ./bspwm.sh
+fi
+
+# If the user selects hyprland, execute the hyprland.sh script
+if echo "$OPTIONS" | grep -q "hyprland"; then
+  exec ./hypr.sh
+fi
+
+# If the user cancels, do nothing
+if [ -z "$OPTIONS" ]; then
+  echo "Installation canceled."
+fi
+
