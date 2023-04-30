1d1d391e2427281f223520c9086e5658f2f0997b
deadendpl <oliwier.czerwi7@gmail.com>
2023-04-30 10:01:54 +0200

    modified:   install/bspwm.sh 	modified:   install/hypr.sh

--- install/hypr.sh
+++ install/hypr.sh
@@ -10,7 +10,7 @@ cd ../../home/.config
 # Check if there are any directories present in ~/.config
 if [[ $(ls -d ~/.config/*/ 2>/dev/null) ]]; then
   # Prompt the user before overwriting folders in ~/.config
-  read -rp "WARNING: ~/config directory is not empty. Proceed with overwriting existing files? [y/N] " confirm
+  read -rp "WARNING: ~/.config directory is not empty. Proceed with overwriting existing files? [y/N] " confirm
   if [[ ! "$confirm" =~ [Yy] ]]; then
     echo "Operation aborted by user." >&2
     exit 1
