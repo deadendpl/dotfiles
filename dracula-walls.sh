4291c50d866510581e9240ce777d29b29aa1ef4f
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-05 15:29:23 +0200

    modified:   dracula-walls.sh 	deleted:    home/.config/rofi/config 	deleted:    home/.config/scripts/wifi.sh 	deleted:    home/.conkyrc

--- dracula-walls.sh
+++ dracula-walls.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
+# making wallpapers directory
 mkdir -p ~/Pictures/drac-walls
 
 cd ~/Pictures
 
+# cloning first wallpaper repo and moving some wallpepers to ~/Pictures/drac-walls
 git clone https://github.com/helpotters/dracula-wallpapers.git
-
 cd dracula-wallpapers/illustrations/cat-and-bats
-
 cp dracula-cat-282a36.png ~/Pictures/drac-walls
-
 cd ../galaxy
-
 cp dracula-galaxy-282a36.png ~/Pictures/drac-walls
-
 cd ../trees-and-mountains
-
 cp dracula-mnt-282a36.png ~/Pictures/drac-walls
 cp dracula-mnt-6272a4.png ~/Pictures/drac-walls
-
 cd ../../waves
-
 cp dracula-waves-44475a.png ~/Pictures/drac-walls
 
+# doing the same but with other wallpapers repo
 cd ~/Pictures
-
 rm -rf dracula-wallpapers
-
 git clone https://github.com/aynp/dracula-wallpapers.git
-
 cd dracula-wallpapers/Art
-
 cp * ~/Pictures/drac-walls
-
 cd "../Programming Languages"
-
 cp * ~/Pictures/drac-walls
