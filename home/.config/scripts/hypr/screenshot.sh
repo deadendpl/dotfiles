ff3619ed2c34052457502eb40ce91f18f1c202ab
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-07 09:49:54 +0200

    changed screenshot for hyprland

--- home/.config/scripts/hypr/screenshot.sh
+++ home/.config/scripts/hypr/screenshot.sh
@@ -3,13 +3,15 @@
 filename=$(date +"%Y-%m-%d_%H-%M-%S".png)
 
 CHOICE=$(echo "Full Screen
-Select Area" | rofi -config ~/.config/rofi/config-bare.rasi -theme ~/.config/rofi/themes/drac-list.rasi -dmenu -i -l 2 -b -p "Screenshot Type:")
+Select Area
+Select Window" | rofi -config ~/.config/rofi/config-bare.rasi -theme ~/.config/rofi/themes/drac-list.rasi -dmenu -i -l 3 -b -p "Screenshot Type:")
 
 if [ "$CHOICE" = "Full Screen" ]; then
-  grimshot --notify save screen ~/Pictures/$filename
+  sleep 0.4s && grimshot --notify save screen ~/Pictures/$filename
 
 elif [ "$CHOICE" = "Select Area" ]; then
-  grimshot --notify save area ~/Pictures/$filename
+  sleep 0.4s && grimshot --notify save area ~/Pictures/$filename
+
 else
   exit
 fi
