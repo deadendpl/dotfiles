6ce02c5a5aa160bdb6521e5a379394d036ede11c
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-09 19:04:47 +0200

    modified:   README.md 	modified:   home/.config/hypr/hyprland.conf 	modified:   home/.config/qutebrowser/config.py 	modified:   home/.config/scripts/themes/catppuccin/ALL.sh 	deleted:    home/.config/scripts/themes/catppuccin/gtk3.sh 	modified:   home/.config/scripts/themes/dracula/ALL.sh 	deleted:    home/.config/scripts/themes/dracula/gtk3.sh

--- home/.config/qutebrowser/config.py
+++ home/.config/qutebrowser/config.py
@@ -196,7 +196,7 @@ import dracula.draw
 
 # Load existing settings made via :set
 config.load_autoconfig()
-# 
+# # 
 dracula.draw.blood(c, {
     'spacing': {
         'vertical': 6,
--- home/.config/scripts/themes/dracula/ALL.sh
+++ home/.config/scripts/themes/dracula/ALL.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-cd ~/.config/scripts/themes/dracula
+directory=~/.config/scripts/themes/dracula
+exclude_script=./ALL.sh
 
-./alacritty.sh
-./doom-emacs.sh
-./dunst-bspwm.sh
-./dunst-hypr.sh
-./foot.sh
-./gtk3.sh
-./gtk4.sh
-./polybar.sh
-./qutebrowser.sh
-./rofi.sh
-./waybar.sh
-./wezterm.sh
+cd "$directory"
 
-echo "It's recommended to logout of your machine re reboot."
+for script in *; do
+  if [ "$script" != "$exclude_script" ] && [ -x "$script" ] && [ -f "$script" ]; then
+    if [ "$script" != "${0##*/}" ]; then
+      ./"$script"
+    fi
+  fi
+done
+
+echo "It's recommended to log out or reboot your machine."
 
 read -p "Press Enter to exit"
