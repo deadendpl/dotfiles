7ef274f99c6796fc546d3ca4748d67dc9612414c
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-14 10:02:02 +0200

    addded catppuccin latte to main theme changing

--- home/.config/scripts/themes/main.sh
+++ home/.config/scripts/themes/main.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 theme=$(printf "Dracula
-Catppuccin Mocha" | rofi -config ~/.config/rofi/config-bare.rasi -l 2 -dmenu -p "Themes")
+Catppuccin Mocha
+Catppuccin Latte" | rofi -config ~/.config/rofi/config-bare.rasi -l 3 -dmenu -p "Themes")
 
 if [ "$theme" == "Dracula" ]; then
    $terminal -e ~/.config/scripts/themes/dracula/ALL.sh
 elif [ "$theme" == "Catppuccin Mocha" ]; then
-   $terminal -e ~/.config/scripts/themes/catppuccin/ALL.sh
+   $terminal -e ~/.config/scripts/themes/catppuccin-mocha/ALL.sh
+elif [ "$theme" == "Catppuccin Latte" ]; then
+   $terminal -e ~/.config/scripts/themes/catppuccin-latte/ALL.sh
 else
    exit
 fi
