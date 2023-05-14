718d7afdb3e3e6bc84b1de490e7510f489c114c1
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-14 18:53:02 +0200

    added zathura

---
+++ home/.config/scripts/themes/dracula/zathura.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file_path="$HOME/.config/zathura/zathurarc"
+
+old_line=$(grep "include" "$file_path")
+
+# Check if the old line exists in the file
+if [ -n "$old_line" ]; then
+    # Replace the line using sed
+    new_line='include dracula'
+    sed -i "s|$old_line|$new_line|" "$file_path"
+    echo "Line replaced successfully!"
+else
+    echo "Old line not found in the file."
+fi
