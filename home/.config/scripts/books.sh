20a069a19e6010f7e9bdd142571dc1258a50459d
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-14 22:40:47 +0200

    added script hub

---
+++ home/.config/scripts/books.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is for reading books that are put in one folder
+
+dir="$HOME/Documents/books"
+cd $dir
+
+book=$(ls | rofi -config ~/.config/rofi/config-bare.rasi -dmenu -p Books)
+
+xdg-open $book
---
+++ home/.config/scripts/hub.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dir="$HOME/.config/scripts"
+
+cd $dir
+
+declare -a scripts=( "Bookmarks" "Books" "Power menu" "Sxhkd help" "Themes" "Web search" )
+
+choice=$(printf '%s\n' "${scripts[@]}" | sort | rofi -config ~/.config/rofi/config-bare.rasi -dmenu -l ${#scripts[@]} -i -b -p "Scripts" )
+
+if [[ $choice == "Bookmarks" ]]; then
+  ./bookmarks.sh
+
+elif [[ $choice == "Books" ]]; then
+  ./books.sh
+
+elif [[ $choice == "Power menu" ]]; then
+  ./power.sh
+
+elif [[ $choice == "Sxhkd help"  ]]; then
+  ./sxhkd-help.sh
+
+elif [[ $choice == "Themes" ]]; then
+  themes/main.sh
+
+elif [[ $choice == "Web search" ]]; then
+  ./web-search.sh
+
+else
+  exit
+fi
