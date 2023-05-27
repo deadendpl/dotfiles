9d5110313c6574411cf1d80c129e31cdfc5491ca
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-27 10:01:27 +0200

    modified:   home/.config/scripts/themes/catppuccin-latte/waybar.sh 	modified:   home/.config/scripts/themes/catppuccin-mocha/waybar.sh 	modified:   home/.config/scripts/themes/dracula/waybar.sh 	renamed:    home/.config/waybar/catppuccin-latte.css -> home/.config/waybar/colors/catppuccin-latte.css 	renamed:    home/.config/waybar/catppuccin-mocha.css -> home/.config/waybar/colors/catppuccin-mocha.css 	renamed:    home/.config/waybar/dracula.css -> home/.config/waybar/colors/dracula.css 	modified:   home/.config/waybar/style.css

--- home/.config/scripts/themes/catppuccin-mocha/waybar.sh
+++ home/.config/scripts/themes/catppuccin-mocha/waybar.sh
@@ -8,7 +8,7 @@ old_line=$(grep "@import" "$file_path")
 # Check if the old line exists in the file
 if [ -n "$old_line" ]; then
     # Replace the line using sed
-    new_line='@import url("./catppuccin-mocha.css");'
+    new_line='@import url("colors/catppuccin-mocha.css");'
     sed -i "s|$old_line|$new_line|" "$file_path"
     echo "Line replaced successfully!"
 else
