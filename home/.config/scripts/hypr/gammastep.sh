7431a7cddf874b4b543882449308831cb90e8b98
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-27 10:11:49 +0200

    modified:   home/.config/scripts/hypr/gammastep.sh

--- home/.config/scripts/hypr/gammastep.sh
+++ home/.config/scripts/hypr/gammastep.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-process="gammastep"
-
-if pgrep -x "$process" > /dev/null; then
+if pgrep -x gammastep > /dev/null; then
     killall gammastep
 else
     gammastep -O 2200 2>/dev/null &
