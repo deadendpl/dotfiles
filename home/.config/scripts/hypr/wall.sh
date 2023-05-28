17160dee0e7e63f1aa6c04a0f28cbe76cb732579
deadendpl <oliwier.czerwi7@gmail.com>
2023-05-28 09:05:53 +0200

    modified:   home/.config/scripts/hypr/wall.sh 	new file:   home/.config/scripts/themes/catppuccin-latte/wall.sh 	new file:   home/.config/scripts/themes/catppuccin-mocha/wall.sh 	new file:   home/.config/scripts/themes/dracula/wall.sh

--- home/.config/scripts/hypr/wall.sh
+++ home/.config/scripts/hypr/wall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-walls_dir="$HOME/Pictures/drac-walls"
+$walls_dir="$HOME/Pictures/drac-walls"
 
 cd "$walls_dir" || exit 1  # Change directory and exit script if it fails
 
---
+++ home/.config/scripts/themes/dracula/wall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+file_path="$HOME/.config/scripts/hypr/wall.sh"
+
+old_line=$(grep "walls_dir=" "$file_path")
+
+if [ -n "$old_line" ]; then
+    # Replace the line using awk
+    new_line='$walls_dir="$HOME/Pictures/drac-walls"'
+    awk -v old_line="$old_line" -v new_line="$new_line" '{ if ($0 == old_line) { print new_line } else { print $0 } }' "$file_path" > temp_file && mv temp_file "$file_path"
+    echo "Line replaced successfully!"
+else
+    echo "Old line not found in the file."
+fi
