ee913065259f25a44ed4065583bf279f82805d81
deadendpl <oliwier.czerwi7@gmail.com>
2023-06-04 10:46:30 +0200

    added gtk3 change for bspwm

---
+++ home/.config/scripts/themes/catppuccin-mocha/gtk3-bspwm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+file_path="$HOME/.config/gtk-3.0/settings.ini"
+
+old_line=$(grep "gtk-theme-name" "$file_path")
+
+if [ -n "$old_line" ]; then
+    # Replace the line using sed
+    new_line="gtk-theme-name=Catppuccin-Mocha-Standard-Red-Dark"
+    sed -i "s|$old_line|$new_line|" "$file_path"
+    echo "Line replaced successfully!"
+else
+    echo "Old line not found in the file."
+fi
+
+old_line=$(grep "gtk-icon-theme-name" "$file_path")
+
+if [ -n "$old_line" ]; then
+    # Replace the line using sed
+    new_line="gtk-icon-theme-name=Papirus-Dark"
+    sed -i "s|$old_line|$new_line|" "$file_path"
+    echo "Line replaced successfully!"
+else
+    echo "Old line not found in the file."
+fi
