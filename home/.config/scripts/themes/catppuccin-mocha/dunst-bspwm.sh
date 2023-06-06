b88a916aa6ac769fb70a71e3f3dade781d5c3ec1
deadendpl <123810408+deadendpl@users.noreply.github.com>
2023-06-06 18:24:59 +0200

    adding theme changing for org config documents

--- home/.config/scripts/themes/catppuccin-mocha/dunst-bspwm.sh
+++ home/.config/scripts/themes/catppuccin-mocha/dunst-bspwm.sh
@@ -17,3 +17,21 @@ if [ -n "$old_line" ]; then
 else
     echo "Old line not found in the file."
 fi
+
+file_path="$HOME/.config/bspwm/README.org"
+
+old_line=$(grep "dunst -config" "$file_path")
+
+if [ -n "$old_line" ]; then
+    # Escape special characters in the old line
+    escaped_old_line=$(sed 's/[\*\.&]/\\&/g' <<< "$old_line")
+
+    # Escape special characters in the new line
+    escaped_new_line=$(sed 's/[\*\.&]/\\&/g' <<< "dunst -config ~/.config/dunst/catppuccin-mocha.conf &")
+
+    # Replace the line using sed
+    sed -i "s|$escaped_old_line|$escaped_new_line|" "$file_path"
+    echo "Line replaced successfully!"
+else
+    echo "Old line not found in the file."
+fi
--- home/.config/scripts/themes/dracula/foot.sh
+++ home/.config/scripts/themes/dracula/foot.sh
@@ -15,6 +15,23 @@ else
     echo "Old line not found in the file."
 fi
 
+file_path="$HOME/.config/hypr/README.org"
+
+# Get the line containing "@import"
+old_line=$(grep "terminal=" "$file_path")
+
+# Check if the old line exists in the file
+if [ -n "$old_line" ]; then
+    # Replace the line using sed
+    new_line='$terminal=foot -c ~/.config/foot/dracula.ini'
+    sed -i "s|$old_line|$new_line|" "$file_path"
+    echo "Line replaced successfully!"
+else
+    echo "Old line not found in the file."
+fi
+
+unset $file_path
+
 unset $file_path
 
 file_path="$HOME/.config/scripts/hypr/wrappedhl"
