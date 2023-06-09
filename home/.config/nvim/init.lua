846e9bf3da2c0126aafba0040c95b42429ba5b9b
deadendpl <123810408+deadendpl@users.noreply.github.com>
2023-06-09 11:55:31 +0200

    .

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -60,6 +60,17 @@ use {
       end
   }
 
+-- org mode
+use {'nvim-treesitter/nvim-treesitter'}
+use {'nvim-orgmode/orgmode', config = function()
+  require('orgmode').setup{}
+end
+}
+
+-- minesweeper
+use {'seandewar/nvimesweeper'}
+
+
 -- [[ Dev ]]
   use {
     'nvim-telescope/telescope.nvim',                 -- fuzzy finder
@@ -205,5 +216,28 @@ require('telescope').setup{
     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
   }
 }
+
+-- org mode setup
+
+-- Load custom treesitter grammar for org filetype
+require('orgmode').setup_ts_grammar()
+
+-- Treesitter configuration
+require('nvim-treesitter.configs').setup {
+  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+  -- highlighting will fallback to default Vim syntax highlighting
+  highlight = {
+    enable = true,
+    -- Required for spellcheck, some LaTex highlights and
+    -- code block highlights that do not have ts grammar
+    additional_vim_regex_highlighting = {'org'},
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
+
+require('orgmode').setup({
+  org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+  org_default_notes_file = '~/Dropbox/org/refile.org',
+})
 
 end)
