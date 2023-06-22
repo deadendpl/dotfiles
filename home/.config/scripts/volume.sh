32692b7d815353874b14f984ac37dd5d973bdb1f
deadendpl <oliwier.czerwi7@gmail.com>
2023-06-22 20:44:55 +0200

    added separate sound changing

---
+++ home/.config/scripts/volume.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Prompt for the volume change using Rofi
+choice=$(rofi -config ~/.config/rofi/config-bare.rasi -dmenu  -l 0 -p "How much to change the volume by?")
+
+# Check if the choice is a valid number
+if [[ $choice =~ ^-?[0-9]+$ ]]; then
+  # Format the choice to include the percentage symbol
+  if [[ $choice -ge 0 ]]; then
+    formatted_choice="+${choice}%"
+  else
+    formatted_choice="${choice}%"
+  fi
+
+  # Adjust the volume using pactl
+  pactl set-sink-volume @DEFAULT_SINK@ "$formatted_choice"
+else
+  echo "Invalid input: $choice"
+fi
