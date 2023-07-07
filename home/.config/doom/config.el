18462699b7aec0bfb971699f4b253b2b43f8bdf2
deadendpl <oliwier.czerwi7@gmail.com>
2023-07-07 09:30:19 +0200

    .

--- home/.config/doom/config.el
+++ home/.config/doom/config.el
@@ -22,7 +22,7 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "CodeNewRoman Nerd Font Mono" :size 13))
-(setq doom-variable-pitch-font (font-spec :family "Ubuntu Nerd Font" :size 13))
+(setq doom-variable-pitch-font (font-spec :family "GoMono Nerd Font" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
