34c85108874f4827cddcab37250015c1395d6c27
deadendpl <oliwier.czerwi@proton.me>
2024-01-09 18:44:01 +0100

    Termux experiments

--- termux/deploy.sh
+++ termux/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 termux-change-repo
 termux-setup-storage
-pkg install emacs sqlite fish eza git openssh openssh-sftp-server iproute2
+pkg --yes install emacs sqlite fish eza git openssh openssh-sftp-server iproute2
 chsh -s fish
+
+rm -rf ~/.config/
+ln -s $PREFIX/../home/.dotfiles/termux/.config $PREFIX/../home/.config
+ln -s $PREFIX/../home/.dotfiles/termux/.termux $PREFIX/../home/.termux
