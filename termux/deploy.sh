7e46245994b7b7890c96864bbc9831b2c62f5706
deadendpl <oliwier.czerwi@proton.me>
2024-03-03 11:07:02 +0100

    Termux test

--- termux/deploy.sh
+++ termux/deploy.sh
@@ -4,6 +4,12 @@ termux-setup-storage
 pkg install --yes emacs sqlite fish eza git openssh openssh-sftp-server iproute2
 chsh -s fish
 
+# linking directories
 rm -rf ~/.config/ ~/.termux/
-ln -s $PREFIX/../home/.dotfiles/termux/.config $PREFIX/../home/
+ln -s $PREFIX/../home/.dotfiles/.config $PREFIX/../home/
 ln -s $PREFIX/../home/.dotfiles/termux/.termux $PREFIX/../home/
+
+# setting up the font
+cd ~/.termux
+wget "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFontMono-Regular.ttf"
+mv JetBrainsMonoNerdFontMono-Regular.ttf font.ttf
